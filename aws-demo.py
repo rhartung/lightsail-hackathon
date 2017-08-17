28fb1aeef2b3dd61542e2a69b906e6a7c9cb2ec1
Robyn Lundin <robyn@hartung.com>
2017-08-17 14:21:47 -0700

    First commit

---
+++ aws-demo.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, jsonify, render_template
+from flask import redirect, request, flash, session
+
+app = Flask(__name__)
+
+app.secret_key = os.environ["SECRET_KEY"]
+
+
+@app.route("/")
+def homepage():
+    """Show super fancy homepage template"""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug
+
+    app.run(port=5005, host="0.0.0.0")
