5e27f7119b6110b09ed7cd3177b6160522dfd3c6
Robyn Lundin <robyn@hartung.com>
2017-08-18 11:03:32 -0700

    added comments to rekognition file WIP

--- rekognition.py
+++ rekognition.py
@@ -5,6 +5,9 @@ import boto3
 client = boto3.client("rekognition")
 s3 = boto3.client("s3")
 
+# testing API response from AWS rekognition
+# response confirms 2 faces are unmatched
+# 99% certainty that both pictures contain faces
 response = client.compare_faces(
     SourceImage={
         "S3Object": {
